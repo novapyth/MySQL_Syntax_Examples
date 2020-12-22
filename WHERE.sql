4d937a37b7ccb942d04e43aa5c6214db6de32871
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-22 09:28:15 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -17,17 +17,17 @@ SELECT GENE_ID
 FROM rnaseq 
 WHERE Gene_length>1000;
 
--- SELECT the column GENE_ID  from the table rnaseq, where Gene_length=1000
+-- SELECT the column GENE_ID  from the table rnaseq, where Gene_length<1000
 SELECT GENE_ID 
 FROM rnaseq 
-WHERE Gene_length=1000;
+WHERE Gene_length<1000;
 
 
 -- SELECT the column Gene_Annotation from the table rnaseq
-SELECT Gene_Annotation  FROM rnaseq;
+SELECT Gene_Annotation FROM rnaseq;
 
--- SELECT the column Expression_values from the table rnaseq
-SELECT Expression_values  FROM rnaseq;
+-- SELECT the column Expression_values from the table rnaseq WHERE Gene_length<2000;
+SELECT Expression_values  FROM rnaseq WHERE Gene_length<2000;
 
 -- SELECT the column Gene_length from the table rnaseq
 SELECT Gene_length FROM rnaseq;
