5df06b85b6a8b91faa36e6df08f27ae3e0b8a1e2
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-22 09:18:29 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -1,6 +1,7 @@
---------------- WHERE Statement
+--------------- WHERE Statement---------------------
 ------Syntax:
 SELECT column_name FROM table_name WHERE condition;
 SELECT column_name(s) FROM table_name WHERE condition;
 SELECT column_name FROM table_name WHERE condition(s);
-SELECT column_name(s) FROM table_name WHERE condition;
+SELECT column_name(s) FROM table_name WHERE condition(s);
+--------------- WHERE Statement---------------------
