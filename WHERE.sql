2800403c3793b767a0599907426948e2f8dbbbcc
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-23 09:36:13 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -222,3 +222,27 @@ SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=6000
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=500;
 
 ------------- <= ------------- Less than or equal -------------
+-- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length>=400;
+SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length>=400;
+-- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length>=500;
+SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length>=500;
+-- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length>=800;
+SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length>=800;
+
+-- SELECT  * from the table rnaseq WHERE Gene_length>=1000;
+SELECT * FROM rnaseq WHERE Gene_length>=1000;
+-- SELECT  * from the table rnaseq WHERE Gene_length>=2000;
+SELECT * FROM rnaseq WHERE Gene_length>=2000;
+
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=1000
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=1000;
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=2000
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=2000;
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=3000
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=3000;
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=4000
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=4000;
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=6000
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=6000;
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM >=500
+SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >=500;
