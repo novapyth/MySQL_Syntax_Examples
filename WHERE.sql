199806b3ec91376ed886735ddeef68e950e23729
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-23 08:42:36 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -99,6 +99,11 @@ SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length=4000;
 
 --- !=, <>	Not equal
 
+--- !=, <>	Not equal
+-- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length!=400;
+SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length!=400;
+-- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length!=500;
+SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length!=500;
 
 --- !=, <>	Not equal
 -- SELECT  columns Gene_ID, RPKM from the table rnaseq WHERE Gene_length<>400;
