e7009df8087f4b4be83c059a514ec4a7fa3631cf
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-23 09:02:33 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -155,9 +155,9 @@ SELECT Gene_ID, RPKM FROM rnaseq WHERE Gene_length>4000;
 -- SELECT  columns Gene_ID, Gene_Annotation, RPKM from the table rnaseq WHERE Gene_length>1000;
 SELECT Gene_ID, Gene_Annotation, RPKM FROM rnaseq WHERE Gene_length>1000;
 -- SELECT  columns Gene_ID, Gene_Annotation, RPKM from the table rnaseq WHERE Gene_length>2000;
-SELECT Gene_ID, Gene_Annotation, RPKM FROM rnaseq WHERE Gene_length<>2000;
--- SELECT  columns Gene_ID, Gene_Annotation, RPKM from the table rnaseq WHERE Gene_length<>5000;
-SELECT Gene_ID, Gene_Annotation, RPKM FROM rnaseq WHERE Gene_length<>5000;
+SELECT Gene_ID, Gene_Annotation, RPKM FROM rnaseq WHERE Gene_length>2000;
+-- SELECT  columns Gene_ID, Gene_Annotation, RPKM from the table rnaseq WHERE Gene_length>5000;
+SELECT Gene_ID, Gene_Annotation, RPKM FROM rnaseq WHERE Gene_length>5000;
 
 -- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM <>1000
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM <>1000;
