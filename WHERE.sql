791aaa35034601adedf6afe66b8d77a00e4b9ef4
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-23 09:06:13 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -171,3 +171,5 @@ SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >4000;
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >5000;
 -- SELECT column Gene_ID, Gene_Annotation, Unique_gene_reads from the table rnaseq WHERE RPKM >6000;
 SELECT Gene_ID, Gene_Annotation, Unique_gene_reads  FROM rnaseq WHERE RPKM >6000;
+
+<	Less than	
