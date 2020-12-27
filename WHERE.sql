0622b2254aa48ccc5dd4b6b13166fd5453ba5416
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-27 09:05:06 +0530

    Update WHERE.sql

--- WHERE.sql
+++ WHERE.sql
@@ -265,7 +265,7 @@ SELECT * FROM rnaseq WHERE Gene_length BETWEEN 500 AND 750;
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM >1000;
 -- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM BETWEEN 250 AND 1000; Tier #2 250 - 1000
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM BETWEEN 250 AND 1000;
--- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM BETWEEN 500 AND 1000;Tier #3 100 - 250
+-- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM BETWEEN 100 AND 250;Tier #3 100 - 250
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM BETWEEN 100 AND 250;
 -- SELECT columns Gene_ID, Gene_Annotation, Gene_length, RPKM  from the table rnaseq WHERE RPKM BETWEEN 500 AND 1000;Tier #4 50 - 100
 SELECT Gene_ID, Gene_Annotation, Gene_length, RPKM FROM rnaseq WHERE RPKM BETWEEN 50 AND 100;
