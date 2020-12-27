3686eb63057cc8874e0a60fa52972d62f5d11f0b
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-27 14:59:37 +0530

    Update Load_Genome_Annotation.sql

--- Load_Genome_Annotation.sql
+++ Load_Genome_Annotation.sql
@@ -1,7 +1,6 @@
 -- Load local infile "~/Scopulariopsis/Genome_Annotation.csv" INTO TABLE Genome_Annotation
-LOAD DATA LOCAL INFILE "~/Scopulariopsis/RNAseq.csv" INTO TABLE scopulariopsis.Genome_Annotation 
+LOAD DATA LOCAL INFILE "~/Scopulariopsis/Genome_Annotation.csv" INTO TABLE scopulariopsis.Genome_Annotation 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES 
-(Gene_ID, Gene_Annotation, Expression_values, Gene_length, Unique_gene_reads, Total_gene_read, RPKM);
-	Protein_Sequence_Length, mean_Similarity, NO_GOs, GOs, Enzyme_Codes
+(Gene_ID, Gene_Annotation, Protein_Sequence_Length, mean_Similarity, NO_GOs, GOs, Enzyme_Codes);
