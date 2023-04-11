796cb97e85dd688550488b16e782875f3010c7ee
novapyth <draabhishekkumar@gmail.com>
2023-04-11 07:10:03 +0200

    Update mysqldb.py

--- mysqldb.py
+++ mysqldb.py
@@ -1,8 +1,10 @@
-
+#  import MySQL connector
 import mysql.connector
+#  mydb info
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="rootpassword"
 )
+#  print mydb
 print(mydb) 
